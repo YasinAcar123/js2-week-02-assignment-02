76bb9001014a0c8da6a6d733c7a31f1fbad06df2
YasinAcar123 <yasin.acar@hicoders.ch>
2022-11-24 17:12:27 +0100

    1

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
-const changeEmailFromGmailToHicoders = () => {};
+const changeEmailFromGmailToHicoders = (pArray) => {
+      let result=pArray.reverse().map((item)=> item.toLowerCase().replace(["@gmail.com"], ["@hicoders.ch"]))
+      return result;
+};
 
-const findSuperNumbers = () => {};
+
+const findSuperNumbers = (pNum) => {
+      const result=pNum.filter((element)=> {
+            let firstDigit= element %10;
+            let secondDigit= Math.floor(element/10) %10
+            let thirdDigit= Math.floor(element/100) %10
+        
+            let total= (Math.pow(firstDigit,3))+ (Math.pow(secondDigit,3))+ (Math.pow(thirdDigit,3))
+            return total=== element
+        })
+  return result
+};
 
 export { changeEmailFromGmailToHicoders, findSuperNumbers };
